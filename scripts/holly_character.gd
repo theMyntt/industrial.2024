9d873ac638da9fbba23db99248a0b76852f96e0c
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-10-21 20:56:54 -0300

    feat: basic lr moviment

---
+++ scripts/holly_character.gd
@@ -0,0 +1,21 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+
+func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
