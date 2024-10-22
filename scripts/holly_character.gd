2f843c7030567923e7b4b1c091ecb91cff574275
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-10-21 21:36:59 -0300

    feat: jump animmation

--- scripts/holly_character.gd
+++ scripts/holly_character.gd
@@ -5,18 +5,26 @@ const JUMP_VELOCITY = -400.0
 
 @onready var animation := $AnimatedSprite2D as AnimatedSprite2D
 
+var isJumping = false
+
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		isJumping = true
+	elif is_on_floor():
+		isJumping = false
 
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
-		animation.play("running")
-		animation.scale.x = direction
+		if !isJumping:
+			animation.play("running")
+		else:
+			animation.play("jump")
+		animation.scale.x = direction 
 	else:
 		animation.play("idle")
 		velocity.x = move_toward(velocity.x, 0, SPEED)
