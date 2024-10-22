b8e5b0b48eb9557bb08936461abfbb1c6858202b
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-10-21 21:32:29 -0300

    feat: running animation

--- scripts/holly_character.gd
+++ scripts/holly_character.gd
@@ -1,9 +1,9 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+@onready var animation := $AnimatedSprite2D as AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
@@ -15,7 +15,10 @@ func _physics_process(delta: float) -> void:
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
+		animation.play("running")
+		animation.scale.x = direction
 	else:
+		animation.play("idle")
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
